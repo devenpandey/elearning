5e963e35c7536655bae18a28718924e045852ee5
devenpandey <deven.pandey@gmail.com>
2020-07-29 03:57:26 +0000

    Adding Content and module functionality

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xkls%^%b*()h-1k&nctac8z5%l2&+f+83)zs@6+4fl9vz0&4z8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.89.155.163']
+ALLOWED_HOSTS = ['techgyan.com','34.207.189.32']
 
 
 # Application definition
